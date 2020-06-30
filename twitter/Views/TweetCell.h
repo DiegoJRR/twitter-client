a8d33fab7500829e75355bef3b34cb9d4086cf40
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-06-30 16:08:37 -0500

    Added TweetCell class

---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,18 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Diego de Jesus Ramirez on 29/06/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *tweetLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
