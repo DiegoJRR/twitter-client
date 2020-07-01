31aee5ac904bce4323470cb6b3f102b2489ffe3f
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-01 17:05:41 -0500

    Added new empty view for composing.
    This view is presented modally, and it can be dismissed with the button. Also added a Logout and Tweet button, which don't do anything yet. UI Changes GIF: http://g.recordit.co/k3HhGubS3r.gif

---
+++ twitter/View Controllers/ComposeViewController.h
@@ -0,0 +1,19 @@
+//
+//  ComposeViewController.h
+//  twitter
+//
+//  Created by Diego de Jesus Ramirez on 01/07/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ComposeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *closeButton;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,36 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Diego de Jesus Ramirez on 01/07/20.
+//  Copyright © 2020 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (IBAction)closeCompose:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
