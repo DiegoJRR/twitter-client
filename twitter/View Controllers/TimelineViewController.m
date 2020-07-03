97410396720c23364cedd6d09a3996ca74732ab3
Diego de Jesus Ramirez <diegojrr@fb.com>
2020-07-02 23:36:13 -0500

    Added a method to the logout button to return to the login page and clear the access credentials

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -12,6 +12,8 @@
 #import "Tweet.h"
 #import "UIImageView+AFNetworking.h"
 #import "ComposeViewController.h"
+#import "AppDelegate.h"
+#import "LoginViewController.h"
 
 @interface TimelineViewController () <ComposeViewControllerDelegate, UITableViewDataSource, UITableViewDelegate>
 
@@ -129,6 +131,16 @@
     
     
     return cell;
+}
+
+- (IBAction)logoutAction:(id)sender {
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
+        
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+    appDelegate.window.rootViewController = loginViewController;
+    
+    [[APIManager shared] logout];
 }
 
 #pragma mark - Navigation
